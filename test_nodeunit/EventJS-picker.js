6887176f5a5723d30b2146b1434907dc91864bd1
Fabian Schmid <fabian.schmid@wca.at>
2015-07-22 01:10:11 +0200

    update require local.crisp-base of node_modules

--- test_nodeunit/EventJS-picker.js
+++ test_nodeunit/EventJS-picker.js
@@ -1,6 +1,6 @@
 
-// require("crisp-base");
-require("../../Crisp.BaseJS/dist/crisp-base");
+require("crisp-base");
+// require("../../Crisp.BaseJS/dist/crisp-base");
 require("../src/EventJS");
 
 module.exports = require("../test/EventJS-picker_test");
